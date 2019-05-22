50928f3929d33e9df965d5029f55c53445851815
Jose Collas <joseherminiocollas@goatstone.com>
2019-05-22 15:28:21 -0700

    add example of useState

--- src/MinHooks.js
+++ src/MinHooks.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MinHooks.css'
 
 function MinHooks() {
+  // local state
+  const [state, setState] = useState('abc')
+
   return (
     <section className="min-hooks">
       <header>
-      MinHooks
+        MinHooks
       </header>
+      { state }
+      <button
+        onClick={ () => setState('xxx') }
+        >
+        set state </button>
     </section>
   )
 }

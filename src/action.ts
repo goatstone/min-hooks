86cc8b97b0d6ad3f3ac67a259bf2c1e51530c594
Jose Collas <joseherminiocollas@goatstone.com>
2019-06-03 12:30:57 -0700

    add action interfaces

--- src/action.ts
+++ src/action.ts
@@ -1,10 +1,22 @@
 export interface IaddWidgetName {
   (widgetName: string): void
 }
+export interface IeditWidgeName {
+  (widgetName: string, newWidgetName: string): void
+}
+export interface IdeleteWidgetName {
+  (widgetName: string): void
+}
+export interface IshowMessage {
+  (message: string): void
+}
+export interface ihideMessage {
+  (): void
+}
 export interface ActionsInterface {
   addWidgetName: IaddWidgetName
-  editWidgetName: (widgetName: string, newWidgetName: string) => void
-  deleteWidgetName: (widgetName: string) => void
-  showMessage: (message: string) => void
-  hideMessage: () => void
+  editWidgetName: IeditWidgeName
+  deleteWidgetName: IdeleteWidgetName
+  showMessage: IshowMessage
+  hideMessage: ihideMessage
 }

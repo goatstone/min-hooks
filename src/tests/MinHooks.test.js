c5d818ebc5980b210101293a7d84177cc86e036d
Jose Collas <joseherminiocollas@goatstonc.com>
2019-06-26 10:50:13 -0700

    update test, fix

--- src/tests/MinHooks.test.js
+++ src/tests/MinHooks.test.js
@@ -17,7 +17,8 @@ describe('<MinHooks />', () => {
     jest.mock('../state', () => ({
       widgetNames: [],
       lastUpdated: '',
-      isShowingMessage: false,
+      isShowingMessage: true,
+      message: 'abc'
     }))
     /* eslint prefer-destructuring: 0 */
     StoreProvider = require('../StoreContext').StoreProvider
@@ -30,14 +31,11 @@ describe('<MinHooks />', () => {
   })
   it('should have a particular shape', () => {
     expect(element.find('section.min-hooks header').length).toBe(1)
-    expect(element.find('section.min-hooks article').length).toBe(4)
-  })
-  it('should respond to a global button show false', () => {
     expect(element.find('article.message-display'))
-    expect(element.find('article.message-display').text()).toBe('')
-
-    element.find('article.message-control button.show-message').simulate('click')
-
-    expect(element.find('article.message-display').text()).toBe('message')
+    expect(element.find('article.message-control'))
+  })
+  it('should contain information from the state', () => {    
+    expect(element.find('article.message-control input').length).toBe(1)
+    expect(element.find('article.message-display').text()).toBe('abc')
   })
 })

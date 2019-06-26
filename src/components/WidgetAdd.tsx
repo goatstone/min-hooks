f121bc451ce588f275ff94401d8229124f22923f
Jose Collas <joseherminiocollas@goatstonc.com>
2019-06-25 18:33:04 -0700

    refactor functions, disable button on empty input

--- src/components/WidgetAdd.tsx
+++ src/components/WidgetAdd.tsx
@@ -6,17 +6,25 @@ const WidgetAdd: React.FC = () => {
   const { state, actions }: StoreContextInterface = React.useContext(StoreContext)
   const [localState, localSetState] = React.useState('')
 
+  const setNameValue = ({ target }: any) => {
+    localSetState(target.value)
+  }
+  const addWidgetName = () => {
+    actions.addWidgetName(localState)
+    localSetState('')
+  }
   return (
     <article className="widget-add-control">
       <h4>Add Widget Name</h4>
       <input
         value={localState}
-        onChange={e => localSetState(e.target.value)}
+        onChange={setNameValue}
       />
       <button
         type="button"
         className="add-widget"
-        onClick={() => actions.addWidgetName(localState)}
+        onClick={addWidgetName}
+        disabled={localState.length === 0}
       >
         Add Widget Name
         {state.lastUpdate}

2602d13b5274782653febe9935591c29ad052ecb
Jose Collas <joseherminiocollas@goatstonc.com>
2019-10-10 15:12:29 -0700

    rewrite test for top level components

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,29 +1,21 @@
-import Enzyme from 'enzyme'
+import React from 'react'
+import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { JSDOM } from 'jsdom'
+import MinHooks from '../components/MinHooks'
+import { StoreProvider } from '../components/StoreContext'
 
 Enzyme.configure({ adapter: new Adapter() })
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
-const { window } = jsdom
 
-beforeEach(() => {
-  jest.resetModules()
-})
-
-describe('index', () => {
-  beforeAll(() => {
-    global.window = window
-    global.document = window.document
-  })
-  it('should have the value set in initState', () => {
-    Object.defineProperty(document, 'getElementById', {
-      value: () => document.createElement('div'),
-    })
+describe('Top Level Components', () => {
+  it('should mount without error', () => {
     try {
-      require('../index')
-    } catch (error) {
-      // fail the test on error
-      expect(false).toBe(true)
+      mount(
+        <StoreProvider>
+          <MinHooks />
+        </StoreProvider>,
+      )
+    } catch (e) {
+      expect(true).toBe(false)
     }
   })
 })

4ccf4b8cc79fc463e1ef8330cf219e16bf3df3ce
Jose Collas <joseherminiocollas@goatstonc.com>
2019-10-10 16:13:28 -0700

    add file to run the testing of the main file

---
+++ src/tests/index-dom.test.js
@@ -0,0 +1,29 @@
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { JSDOM } from 'jsdom'
+
+Enzyme.configure({ adapter: new Adapter() })
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+const { window } = jsdom
+
+beforeEach(() => {
+  jest.resetModules()
+})
+
+describe('index', () => {
+  beforeAll(() => {
+    global.window = window
+    global.document = window.document
+  })
+  it('should run file that mounts top level components', () => {
+    Object.defineProperty(document, 'getElementById', {
+      value: () => document.createElement('div'),
+    })
+    try {
+      require('../index')
+    } catch (error) {
+      // fail the test on error
+      expect(false).toBe(true)
+    }
+  })
+})

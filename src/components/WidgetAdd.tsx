ec61552083da6d54e905ab952a4888f152f38c50
Jose Collas <joseherminiocollas@goatstonc.com>
2019-10-11 10:53:08 -0700

    refactor WidgetAdd

--- src/components/WidgetAdd.tsx
+++ src/components/WidgetAdd.tsx
@@ -1,38 +1,40 @@
 import React from 'react'
-import { StoreContext, StoreContextInterface } from './StoreContext'
+import { StoreContext } from './StoreContext'
 import './WidgetAdd.css'
 
 const WidgetAdd: React.FC = () => {
-  const { actions }: StoreContextInterface = React.useContext(StoreContext)
   const [widgetName, setWidgetName] = React.useState('')
-  // set global value
-  const addWidgetName = () => {
-    actions.addWidgetName(widgetName)
-    setWidgetName('')
-  }
-  // maintain local values
-  const setNameValue = (name: string) => setWidgetName(name)
 
   return (
-    <article className="widget-add-control">
-      <h4>
-        Add Widget Name
-      </h4>
-      <fieldset>
-        <input
-          value={widgetName}
-          onChange={e => setNameValue(e.target.value)}
-        />
-        <button
-          type="button"
-          className="add-widget"
-          onClick={addWidgetName}
-          disabled={widgetName.length === 0}
-        >
-          Add
-        </button>
-      </fieldset>
-    </article>
+    <StoreContext.Consumer>
+      {({ actions }) => (
+        <article className="widget-add-control">
+          <h4>
+            Add Widget Name
+          </h4>
+          <fieldset>
+            <input
+              value={widgetName}
+              onChange={e => setWidgetName(e.target.value)}
+            />
+            <button
+              type="button"
+              className="add-widget"
+              onClick={
+                () => {
+                  actions.addWidgetName(widgetName)
+                  setWidgetName('')
+                }
+              }
+              disabled={widgetName.length === 0}
+            >
+              Add
+            </button>
+          </fieldset>
+        </article>
+      )
+      }
+    </StoreContext.Consumer>
   )
 }
 

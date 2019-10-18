58dad63e877bd0201a3cf189997cc9847da0ad45
Jose Collas <joseherminiocollas@goatstonc.com>
2019-10-18 08:58:18 -0700

    fix lint for file

--- src/components/Message/MessageDisplay.tsx
+++ src/components/Message/MessageDisplay.tsx
@@ -2,18 +2,15 @@ import React from 'react'
 import { StoreContext } from '../StoreContext'
 import './MessageDisplay.css'
 
-const MessageDisplay: React.FC = () => {
-  return (
-    <StoreContext.Consumer>
-      {({ state }) =>
-        (
-          <article className="message-display">
-            {state.isShowingMessage ? <p>{state.message}</p> : ''}
-          </article>
-        )
-      }
-    </StoreContext.Consumer>
-  )
-}
+const MessageDisplay: React.FC = () => (
+  <StoreContext.Consumer>
+    {({ state }) => (
+      <article className="message-display">
+        {state.isShowingMessage ? <p>{state.message}</p> : ''}
+      </article>
+    )
+    }
+  </StoreContext.Consumer>
+)
 
 export default MessageDisplay

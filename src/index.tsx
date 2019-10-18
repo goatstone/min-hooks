7c4a54dad26f633829bb1c1664180701c0411de3
Jose Collas <joseherminiocollas@goatstonc.com>
2019-10-18 09:00:26 -0700

    refactor, remove CSS file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import MinHooks from './components/MinHooks'
 import { StoreProvider } from './components/StoreContext'
 
